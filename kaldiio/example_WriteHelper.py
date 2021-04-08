74c24949c7f3693c7234438ddb77384a51c3b84a
kbh <nine4409@sogang.ac.kr>
2021-04-08 19:11:38 +0900

    kaldiio

---
+++ kaldiio/example_WriteHelper.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+import glob, os
+from kaldiio import WriteHelper
+
+data_list = [x for x in glob.glob(os.path.join("../data2/","*.pt")) if not os.path.isdir(x)]
+
+
+print(data_list)
+
+with WriteHelper('ark,scp:output.ark,output.scp',compression_method=2) as writer :
+    for data_path in data_list:
+        data = torch.load(data_path)
+        data = data.numpy()
+        writer(data_path,data)
